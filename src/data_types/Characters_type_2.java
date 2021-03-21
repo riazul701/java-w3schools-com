3aaa2af0f38c0855fb1b0455760a47d755a8275f
Riazul Hasan <riazul701@gmail.com>
2021-03-21 19:41:29 +0600

    Initial Commit.

---
+++ src/data_types/Characters_type_2.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    char a = 65, b = 66, c = 67;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+  }
+}
---
+++ src/home/Example_1.java
@@ -0,0 +1,7 @@
+package home;
+
+public class Example_1 {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
